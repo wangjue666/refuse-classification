0baee8e8289ef14d024e35afa4ae79f4319d4f93
王珏 <wangjuesix@gmail.com>
2020-11-16 11:39:07 +0800

    使用截断模型+双层神经网络来定义模型结构

--- train/index.js
+++ train/index.js
@@ -2,9 +2,26 @@ const tf = require('@tensorflow/tfjs-node');
 const getData = require("./data")
 const TRAIN_DIR = '../垃圾分类/train'
 const OUTPUT_DIR = '../output'
+const MOBILENET_URL = 'http://ai-sample.oss-cn-hangzhou.aliyuncs.com/pipcook/models/mobilenet/web_model/model.json';
 const main = async()=>{
+    //加载数据
     const {xs, ys, classes} = await getData(TRAIN_DIR, OUTPUT_DIR)
     console.log(xs, ys,classes)
+
+    //定义模型
+    const mobilenet = await tf.loadLayersModel(MOBILENET_URL)
+    mobilenet.summary()
+    const model = tf.sequential()
+    for(let i= 0;i<=86;i++){
+        const layer = mobilenet.layers[i]
+        layers.trainable = false
+        model.add(layer)
+    }
+    model.add(tf.layers.flatten())
+    model.add(tf.layers.dense({
+        units: 10,
+        activation: 'relu'
+    }))
 }
 
 main()
