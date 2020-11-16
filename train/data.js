f2fc47c33faf8e1af174d81f54e145f15b3048d3
王珏 <wangjuesix@gmail.com>
2020-11-16 11:23:49 +0800

    加载垃圾分类训练数据

--- train/data.js
+++ train/data.js
@@ -1,5 +1,39 @@
-const getData = async()=>{
-    
+const fs =require("fs")
+const tf = require('@tensorflow/tfjs-node');
+const img2x = (buffer)=>{
+    return tf.tidy(()=>{
+        const imgTs = tf.node.decodeImage(new Uint8Array(buffer))
+        const imgTsResized = tf.image.resizeBilinear(imgTs, [224,224])
+        return imgTsResized.toFloat().sub(255/2).div(255/2).reshape([1,224,224,3])
+    })
+   
+}
+
+const getData = async(trainDir, outputDir)=>{
+    const classes = fs.readdirSync(trainDir)
+    fs.writeFileSync(`${outputDir}/classes.json`, JSON.stringify(classes))
+    const inputs = []
+    const labels = []
+
+    classes.forEach((dir, dirIndex)=>{
+        fs.readdirSync(`${trainDir}/${dir}`)
+        .filter(n=>n.match(/jpg$/))
+        .slice(0,100)
+        .forEach(filename=>{
+            const imgPath = `${trainDir}/${dir}/${filename}`
+            const buffer = fs.readFileSync(imgPath)
+            const x = img2x(buffer)
+            inputs.push(x)
+            labels.push(dirIndex)
+        })
+    })
+
+    const xs = tf.concat(inputs)
+    const ys = tf.tensor(labels)
+    return {
+        xs, ys, classes
+    }
+
 }
 
 
--- train/index.js
+++ train/index.js
@@ -1,9 +1,10 @@
 const tf = require('@tensorflow/tfjs-node');
 const getData = require("./data")
-
-
+const TRAIN_DIR = '../垃圾分类/train'
+const OUTPUT_DIR = '../output'
 const main = async()=>{
-    await getData()
+    const {xs, ys, classes} = await getData(TRAIN_DIR, OUTPUT_DIR)
+    console.log(xs, ys,classes)
 }
 
 main()
